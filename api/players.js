4bc97c230cdd25e1a6509975bbf514a839c9ed50
anastasiakezia <anastasiakezia08@gmail.com>
2025-10-28 11:07:07 +0700

    check log players.js

--- api/players.js
+++ api/players.js
@@ -1,3 +1,16 @@
+export default async function handler(req, res) {
+  console.log("Running /api/players");  // ← ini harus muncul di Vercel logs
+  try {
+    const players = await Players.findAll();
+    res.status(200).json(players);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+
+
 // api/players.js
 import { sequelize } from "./db.js";
 import { Players } from "./models.js";
